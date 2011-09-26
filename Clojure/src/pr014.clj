5f9fa4dc451fb3f0cdb630993be769d3c3401fcd
Michael John Slater <michaeljohnslater@gmail.com>
2011-09-26 17:05:28 -0500

    made problem 14 more efficient

--- Clojure/src/pr014.clj
+++ Clojure/src/pr014.clj
@@ -1,18 +1,11 @@
-
-
 (defn specseq [num]
   (loop [curNum num numsCount 0]
     (if (= curNum 1) (inc numsCount)
       (if (even? curNum)
         (recur (/ curNum 2) (inc numsCount))
-        (recur (inc (* curNum 3)) (inc numsCount))
-        )
-      )
-    )
-  )
+        (recur (inc (* curNum 3)) (inc numsCount))))))
 
-(def natural-nums (iterate inc 1))
-
-(time (let [crazyMap (zipmap (map specseq (take 1000000 natural-nums)) (take 1000000 natural-nums))]
-  (println (get crazyMap (apply max (keys crazyMap))))
-  ))
+(println (time
+  (let [natural-nums (range 1 1000000)]
+    (let [crazyMap (zipmap (map specseq natural-nums) natural-nums)]
+      (get crazyMap (apply max (keys crazyMap)))))))
